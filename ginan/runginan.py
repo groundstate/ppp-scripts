336233cd28ebb10393e4f88a3881eae8ed2c1c6b
groundstate <michaeljwouters@gmail.com>
2024-11-05 22:10:37 +1100

    Multi-day runs can now be set up

--- ginan/runginan.py
+++ ginan/runginan.py
@@ -116,13 +116,21 @@ def EditCfg(cfg):
 		else:
 			cfg[c] = EditCfgData(cfg[c])
 	return cfg
-	
+
+# ------------------------------------------
+def ScrubDir(theDir):
+	if not(args.debug):
+		ottp.Debug(f'Cleaning {theDir}')
+		files = glob.glob(os.path.join(theDir,'*'))
+		for f in files:
+			if os.path.isfile(f):
+				os.unlink(f)
+		
 # --------------------------------------------------------------------------------------------------------
 
 home = os.environ['HOME'] 
 root = home 
 configFile = os.path.join(root,'etc','runginan.yaml')
-tmpDir = os.path.join(root,'tmp')
 
 peaRoot = './'
 peaExec = 'pea'
@@ -141,6 +149,7 @@ parser = argparse.ArgumentParser(description='Generate a station clock solution 
 	formatter_class=argparse.RawDescriptionHelpFormatter,epilog=examples)
 
 parser.add_argument('mjd',nargs = '*',help='first MJD [last MJD]')
+parser.add_argument('--daily',help='generate daily files',action='store_true')
 parser.add_argument('--config','-c',help='use an alternate configuration file',default=configFile)
 parser.add_argument('--debug','-d',help='debug (to stderr)',action='store_true')
 parser.add_argument('--version','-v',action='version',version = os.path.basename(sys.argv[0])+ ' ' + VERSION + '\n' + 'Written by ' + AUTHORS)
@@ -176,6 +185,12 @@ if 'station' in cfg['pea']:
 cfg = EditCfg(cfg) # make substitutions
 
 runDir = cfg['pea']['run_dir']
+if not os.path.isdir(runDir):
+	os.mkdir(runDir)
+tmpDir = os.path.join(runDir,'tmp') # give each run its own temporary directory
+if not os.path.isdir(tmpDir):
+	os.mkdir(tmpDir)
+	
 satDataSrcDir  = cfg['inputs']['satellite_data']['src_dir']
 outputClockDir = cfg['outputs']['clocks']['directory']
 srcDir = cfg['inputs']['gnss_observations']['rnx_src_dir']
@@ -221,79 +236,175 @@ fin.close()
 
 gCfg = EditCfg(gCfg)
 
+gCfg['receiver_options'][rnxStation4Letter] = {}
+gCfg['receiver_options'][rnxStation4Letter]['receiver_type']    = cfg['receiver_options']['receiver_type']
+gCfg['receiver_options'][rnxStation4Letter]['antenna_type']     = cfg['receiver_options']['antenna_type']
+gCfg['receiver_options'][rnxStation4Letter]['apriori_position'] = cfg['receiver_options']['apriori_position']
+	
 dstDir = gCfg['inputs']['gnss_observations']['gnss_observations_root']
-if not os.path.isdir(dstDir):
+if not os.path.isdir(dstDir): # will just be runDir anyway
 	os.mkdir(dstDir)
 	
 stationID = cfg['pea']['station']
 clkFileTemplate = gCfg['outputs']['clocks']['filename'] # save this, because it will be repeatedly modified
 
-satData = [['clk_files',clkTemplate],['bsx_files',bsxTemplate],['sp3_files',sp3Template]]
+satData = [['clk_files',clkTemplate,''],['bsx_files',bsxTemplate,''],['sp3_files',sp3Template,'']]
 
-for mjd in range(startMJD,stopMJD+1):
-	
-	# Scrub the 'run' directory
-	if not(args.debug):
-		ottp.Debug(f'Cleaning {runDir}')
-		files = glob.glob(os.path.join(runDir,'*'))
+if args.daily:
+	for mjd in range(startMJD,stopMJD+1):
 		
-	(yyyy,doy,mon) = ottp.MJDtoYYYYDOY(mjd)
-	yy = yyyy % 100
-	
-	# Do any necessary preprocessing of the station observation files, including decompression
-	# First, find the file
-	
-	(baseObsPath, compressionExt) = rinex.FindObservationFile(srcDir,rnxStation,yyyy,doy,3,True)
-	obsPath = baseObsPath + compressionExt # note that compression extensions are .gz and .Z, not .crx 
-	obsBaseName = os.path.basename(obsPath)
-	
-	# editrnxobs.py will decompress for us but then helpfully (and unnecessarily) recompress it
-	# So give it a decompressed file, which it will leave as is
-	#
-	# Copy the RINEX file to the temporary directory
-	# This is because we might be using files from another user's directory where
-	# we don't have permission to decompress in place 
-	# (and this use case is not supported by editrnxobs.py at present either)
+		ScrubDir(runDir)
+		ScrubDir(tmpDir)
 		
-	shutil.copy(obsPath,tmpDir)
-	obsDecompressedPath,algo = rinex.Decompress(os.path.join(tmpDir,obsBaseName)) # this will be deleted so no need to save details for recompression
-	obsDecompressedBaseName = os.path.basename(obsDecompressedPath)
+		(yyyy,doy,mon) = ottp.MJDtoYYYYDOY(mjd)
+		yy = yyyy % 100
+		
+		ottp.Debug(f'Processing MJD={mjd} yyyy={yyyy} doy={doy}')
+		
+		# Do any necessary preprocessing of the station observation files, including decompression
+		# First, find the file
+		
+		(baseObsPath, compressionExt) = rinex.FindObservationFile(srcDir,rnxStation,yyyy,doy,3,True)
+		obsPath = baseObsPath + compressionExt # note that compression extensions are .gz and .Z, not .crx 
+		obsBaseName = os.path.basename(obsPath)
+		
+		# editrnxobs.py will decompress for us but then helpfully (and unnecessarily) recompress it
+		# So give it a decompressed file, which it will leave as is
+		#
+		# Copy the RINEX file to the temporary directory
+		# This is because we might be using files from another user's directory where
+		# we don't have permission to decompress in place 
+		# (and this use case is not supported by editrnxobs.py at present either)
+			
+		shutil.copy(obsPath,tmpDir)
+		obsDecompressedPath,algo = rinex.Decompress(os.path.join(tmpDir,obsBaseName)) # this will be deleted so no need to save details for recompression
+		obsDecompressedBaseName = os.path.basename(obsDecompressedPath)
+		
+		ottp.Debug('Running ' + editRnxObs)
+		try:
+			cmdargs = [editRnxObs,'--tmpdir',tmpDir,'--excludegnss',exclusions,'--output',dstDir,obsDecompressedPath]
+			# print(cmdargs) # FIXME useful when debugging
+			x = subprocess.check_output(cmdargs) 
+		except Exception as e:
+			print(e)
+			ottp.ErrorExit('Failed to run ' + editRnxObs)
+		
+		ginanInputRINEX = os.path.join(dstDir,obsDecompressedBaseName)
+		if not(os.path.exists(ginanInputRINEX)): # hmmm no output
+			ottp.Debug(f'Expected {ginanInputRINEX} - not found!')
+			continue # not fatal
+		
+		# Customize the Ginan config after finding the file because we need the decompressed base name
+		# and write it out
+		gCfg['inputs']['gnss_observations']['rnx_inputs'] = [obsDecompressedBaseName] # pea grumbles if this is not a list, so give it a list
+		
+		for sd in satData:
+			dstDir = os.path.dirname(gCfg['inputs']['satellite_data'][sd[0]][0]) # destination directory
+			fileName = MJDtoIGSProductName(mjd,sd[1])
+			gCfg['inputs']['satellite_data'][sd[0]] = [os.path.join(dstDir,fileName)]
+			# lazy find
+			files = glob.glob(os.path.join(satDataSrcDir,fileName)+'*')
+			if files:
+				shutil.copy(files[0],dstDir)
+				rinex.Decompress(os.path.join(dstDir,os.path.basename(files[0])))
+			else:
+				pass # FIXME what do we do
+
+		clkFile = clkFileTemplate
+		if 'YYDDD' in clkFile:
+			clkFile = clkFile.replace('YYDDD',f'{yy:02d}{doy:03d}',1)
+			gCfg['outputs']['clocks']['filename']  =  clkFile
+		elif 'DDD0.YY' in clkFile:
+			clkFile = clkFile.replace('DDD0.YY',f'{doy:03d}{yy:02d}',1)
+			gCfg['outputs']['clocks']['filename']  =  clkFile
+		
+		# FIXME unlink this always ?
+		gCfgOut = os.path.join(runDir,'pppclk.yaml')
+		try:
+			fout = open(gCfgOut, 'w')
+		except:
+			ErrorExit('Unable to open ' + gCfgOut)
+			
+		yaml.safe_dump(gCfg, fout, sort_keys = False) # We preserve the input order. Note that comments are stripped
+		
+		# Run 'pea'
+		ottp.Debug('Running pea')
+		tstart = time.time()
+		try:
+			cmdargs = [peaExec,'-y',gCfgOut]
+			subprocess.check_output(cmdargs) 
+		except Exception as e:
+			print(e)
+			ottp.ErrorExit('Failed to run ' + peaExec)
+		tstop = time.time()
+		ottp.Debug('..done')
+		ottp.Debug('Job run time = {:g} s'.format(tstop-tstart))
+		
+		# Collect the output, namely the smoothed clock file
+		# Ginan appears to put _smoothed before the extension
+		base,ext = os.path.splitext(clkFile)
+		clkPath = os.path.join(gCfg['outputs']['clocks']['directory'],f'{base}_smoothed{ext}')
+		shutil.copy(clkPath,os.path.join(outputClockDir,clkFile))
+		ottp.Debug(f'CLK file in {outputClockDir}/{clkFile}')
+				
+else: # output a single CLK file
 	
+	# This all could be done inside the loop for 'daily', but it's easier to followif we don't do that
+	ScrubDir(runDir)
+	ScrubDir(tmpDir)
+	
+	gCfg['inputs']['gnss_observations']['rnx_inputs'] = []
+	
+	for i in range(0,len(satData)):
+		satData[i][2] = os.path.dirname(gCfg['inputs']['satellite_data'][satData[i][0]][0]) # save the directory name 
+		gCfg['inputs']['satellite_data'][satData[i][0]]=[] # and then zero it out
+	
+	for mjd in range(startMJD,stopMJD+1):
+		
+		(yyyy,doy,mon) = ottp.MJDtoYYYYDOY(mjd)
+		yy = yyyy % 100
+		
+		(baseObsPath, compressionExt) = rinex.FindObservationFile(srcDir,rnxStation,yyyy,doy,3,True)
+		obsPath = baseObsPath + compressionExt # note that compression extensions are .gz and .Z, not .crx 
+		obsBaseName = os.path.basename(obsPath)
+		
+		shutil.copy(obsPath,tmpDir)
+		obsDecompressedPath,algo = rinex.Decompress(os.path.join(tmpDir,obsBaseName)) # this will be deleted so no need to save details for recompression
+		obsDecompressedBaseName = os.path.basename(obsDecompressedPath)
+		if (mjd==startMJD):
+			firstFile = obsDecompressedPath
+			dstRnx = obsDecompressedBaseName
+		if (mjd==stopMJD):
+			lastFile  = obsDecompressedPath
+			
+		for sd in satData:
+			dstDir = sd[2] # destination directory, wot we saved
+			fileName = MJDtoIGSProductName(mjd,sd[1])
+			gCfg['inputs']['satellite_data'][sd[0]].append(os.path.join(dstDir,fileName))
+			# lazy find
+			files = glob.glob(os.path.join(satDataSrcDir,fileName)+'*')
+			if files:
+				shutil.copy(files[0],dstDir)
+				rinex.Decompress(os.path.join(dstDir,os.path.basename(files[0])))
+			else:
+				pass # FIXME what do we do
+
 	ottp.Debug('Running ' + editRnxObs)
+	ginanInputRINEX = os.path.join(dstDir,dstRnx)
 	try:
-		cmdargs = [editRnxObs,'--tmpdir',tmpDir,'--excludegnss',exclusions,'--output',dstDir,obsDecompressedPath]
-		# print(cmdargs) # FIXME useful when debugging
+		cmdargs = [editRnxObs,'--catenate','--tmpdir',tmpDir,'--excludegnss',exclusions,'--output',ginanInputRINEX,firstFile,lastFile]
 		x = subprocess.check_output(cmdargs) 
 	except Exception as e:
 		print(e)
 		ottp.ErrorExit('Failed to run ' + editRnxObs)
 	
-	ginanInputRINEX = os.path.join(dstDir,obsDecompressedBaseName)
 	if not(os.path.exists(ginanInputRINEX)): # hmmm no output
-		ottp.Debug(f'Expected {ginanInputRINEX} - not found!')
-		continue # not fatal
-	
-	# Customize the Ginan config after finding the file because we need the decompressed base name
-	# and write it out
-	gCfg['inputs']['gnss_observations']['rnx_inputs'] = [obsDecompressedBaseName] # pea grumbles if this is not a list, so give it a list
-	
-	for sd in satData:
-		dstDir = os.path.dirname(gCfg['inputs']['satellite_data'][sd[0]][0]) # destination directory
-		fileName = MJDtoIGSProductName(mjd,sd[1])
-		gCfg['inputs']['satellite_data'][sd[0]] = [os.path.join(dstDir,fileName)]
-		# lazy find
-		files = glob.glob(os.path.join(satDataSrcDir,fileName)+'*')
-		if files:
-			shutil.copy(files[0],dstDir)
-			rinex.Decompress(os.path.join(dstDir,os.path.basename(files[0])))
-		else:
-			pass # FIXME what do we do
+		ottp.ErrorExit(f'Expected {ginanInputRINEX} - not found!')
 		
-	gCfg['receiver_options'][rnxStation4Letter] = {}
-	gCfg['receiver_options'][rnxStation4Letter]['receiver_type']    = cfg['receiver_options']['receiver_type']
-	gCfg['receiver_options'][rnxStation4Letter]['antenna_type']     = cfg['receiver_options']['antenna_type']
-	gCfg['receiver_options'][rnxStation4Letter]['apriori_position'] = cfg['receiver_options']['apriori_position']
-	
+	gCfg['inputs']['gnss_observations']['rnx_inputs'] = [ginanInputRINEX] # pea grumbles if this is not a list, so give it a list
+		
+	(yyyy,doy,mon) = ottp.MJDtoYYYYDOY(startMJD)
+	yy = yyyy % 100
 	clkFile = clkFileTemplate
 	if 'YYDDD' in clkFile:
 		clkFile = clkFile.replace('YYDDD',f'{yy:02d}{doy:03d}',1)
@@ -309,17 +420,21 @@ for mjd in range(startMJD,stopMJD+1):
 	except:
 		ErrorExit('Unable to open ' + gCfgOut)
 		
-	yaml.safe_dump(gCfg, fout, sort_keys = False) # We preserve the input order. Note that comments are stripped
+	yaml.safe_dump(gCfg, fout, sort_keys = False) 
+	ottp.Debug(f'Wrote {gCfgOut}')
 	
 	# Run 'pea'
 	ottp.Debug('Running pea')
+	tstart = time.time()
 	try:
 		cmdargs = [peaExec,'-y',gCfgOut]
 		subprocess.check_output(cmdargs) 
 	except Exception as e:
 		print(e)
 		ottp.ErrorExit('Failed to run ' + peaExec)
+	tstop = time.time()
 	ottp.Debug('..done')
+	ottp.Debug('Job run time = {:g} s'.format(tstop-tstart))
 	
 	# Collect the output, namely the smoothed clock file
 	# Ginan appears to put _smoothed before the extension
@@ -327,11 +442,3 @@ for mjd in range(startMJD,stopMJD+1):
 	clkPath = os.path.join(gCfg['outputs']['clocks']['directory'],f'{base}_smoothed{ext}')
 	shutil.copy(clkPath,os.path.join(outputClockDir,clkFile))
 	ottp.Debug(f'CLK file in {outputClockDir}/{clkFile}')
-	
-	# Clean up
-	ottp.Debug('Cleaning up ...')
-	os.unlink(obsDecompressedPath) # this is unedited RINEX, which will be in 'tmp'
-	
-	# Delete RINEX files in the pea directory
-	if not(args.debug):
-		os.unlink(ginanInputRINEX)
